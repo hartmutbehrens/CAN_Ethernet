f950f3e9da69ee7e1042fdf92d593fb1ef5bc2f5
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-11-27 22:03:35 +0200

    more sane define ordering

--- config.h
+++ config.h
@@ -11,9 +11,9 @@ typedef union
 #ifndef DHCP_EXPIRE_TIMER_SECS
 #define DHCP_EXPIRE_TIMER_SECS 45			// Timeout for DHCP address request
 #endif
-#define RING_BUF_SIZE (CAN_FRAME_SIZE*16*10)             // size of ring buffer
-#define CAN_FIFO_SIZE   (8 * 8)             // size of FIFO buffers allocated to the CAN controller - 8 messages can each hold a max of 8 bytes
 #define CAN_FRAME_SIZE 14                   // size of CAN frame to be sent via UDP
+#define RING_BUF_SIZE (CAN_FRAME_SIZE*160)  // size of ring buffer
+#define CAN_FIFO_SIZE   (8 * 8)             // size of FIFO buffers allocated to the CAN controller - 8 messages can each hold a max of 8 bytes
 #define CAN_BITRATE 1000000                 // set CAN bitrate to 1Mbps
 #define EXT_FLAG_POS 12                     // position of CAN message ID flag in CAN frame
 #define RTR_FLAG_POS 13                     // position of CAN remote transmission flag in CAN frame
