a8dc3aa33ea387f46a1ba7aa960c571ae7c9c543
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-12-04 21:50:42 +0200

    remove unnecessary function declaration

--- c2e_main.h
+++ c2e_main.h
@@ -8,7 +8,6 @@ static uint32_t INT_init(void);
 static uint32_t LWIP_init(void);
 static uint32_t NETIF_init(void);
 static uint32_t IPaddr_init(void);
-void display_can_statistics(uint32_t msg_count, uint32_t lost_count, uint32_t col, uint32_t row);	// display CAN message statistics
 void PENDSV_handler(void);																			// PendSV interrup handler - causes UDP packets with embedded CAN frames to be sent. Triggered in SW when RingBuffer reaches a threshold.
 
 #endif
