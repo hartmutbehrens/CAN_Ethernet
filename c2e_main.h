6cb720b23c10963aa9972580ac42f4cf7a2ad32c
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-12-04 21:52:03 +0200

    improve comments

--- c2e_main.h
+++ c2e_main.h
@@ -3,11 +3,11 @@
 #include <stdint.h>
 
 static uint32_t display_ip_address(void);							// display an lwIP address
-static uint32_t fsm_error(void);																			// catch all state machine function
-static uint32_t INT_init(void);
-static uint32_t LWIP_init(void);
-static uint32_t NETIF_init(void);
-static uint32_t IPaddr_init(void);
-void PENDSV_handler(void);																			// PendSV interrup handler - causes UDP packets with embedded CAN frames to be sent. Triggered in SW when RingBuffer reaches a threshold.
+static uint32_t fsm_error(void);									// catch all state machine function
+static uint32_t INT_init(void);										// Master interrupt enable
+static uint32_t LWIP_init(void);									// initialize lwIP
+static uint32_t NETIF_init(void);									// get netif
+static uint32_t IPaddr_init(void);									// get ip address
+void PENDSV_handler(void);											// PendSV interrup handler - causes UDP packets with embedded CAN frames to be sent. Triggered in SW when RingBuffer reaches a threshold.
 
 #endif
